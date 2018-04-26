9bb0678bd2728fae5a5a6e1f45c7e935a2a59bba
jonijyl <joni.jyllinmaa@metropolia.fi>
2018-04-26 10:40:14 +0300

    skasodsaokpkdaso

---
+++ src/main/java/org/example/websocket/SessionHandler.java
@@ -0,0 +1,38 @@
+
+package org.example.websocket;
+
+import java.io.IOException;
+import javax.enterprise.context.ApplicationScoped;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.websocket.Session;
+
+//import org.example.model.Device;
+
+@ApplicationScoped
+public class SessionHandler {
+    private final Set<Session> sessions = new HashSet<>();
+    //private final Set<Device> devices = new HashSet<>();
+    
+    public void addSession(Session session) {
+        sessions.add(session);
+    }
+
+    public void removeSession(Session session) {
+        sessions.remove(session);
+    }
+    
+    private void sendToSession(Session session, String message) {
+        try {
+            session.getBasicRemote().sendText(message);
+        } catch (IOException ex) {
+            sessions.remove(session);
+            Logger.getLogger(SessionHandler.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    private void sendToAllConnectedSessions(String message) {
+    }
+}
--- src/main/java/org/example/websocket/WebSocketServer.java
+++ src/main/java/org/example/websocket/WebSocketServer.java
@@ -1,14 +1,52 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package org.example.websocket;
 
-/**
- *
- * @author Sami
- */
+
+
+import java.io.IOException;
+import static java.lang.System.console;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.enterprise.context.ApplicationScoped;
+import javax.websocket.server.ServerEndpoint;
+import javax.websocket.OnClose;
+import javax.websocket.OnError;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.inject.Inject;
+
+@ApplicationScoped
+@ServerEndpoint("/actions")
 public class WebSocketServer {
+    @Inject
+    private SessionHandler sessionHandler;
     
+     @OnOpen
+        public void open(Session session) {
+            System.out.println("-----------------!!!!!!!!!!!!!_----------------------");
+            
+            sessionHandler.addSession(session);
+    }
+
+    @OnClose
+        public void close(Session session) {
+            sessionHandler.removeSession(session);
+    }
+
+    @OnError
+        public void onError(Throwable error) {
+            Logger.getLogger(WebSocketServer.class.getName()).log(Level.SEVERE, null, error);
+    }
+
+    @OnMessage
+        public void handleMessage(String message, Session session) {
+            try {
+            session.getBasicRemote().sendText(message);
+        } catch (IOException ex) {
+            
+            Logger.getLogger(SessionHandler.class.getName()).log(Level.SEVERE, null, ex);
+        }
+            System.out.println("moro");
+    }
 }
