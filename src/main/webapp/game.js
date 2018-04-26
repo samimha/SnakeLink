b78a8418038c397b15d73cc539810e7486102250
Lauri Niemi <niemi.late@gmail.com>
2018-04-26 15:43:25 +0300

    heroku path

--- src/main/webapp/game.js
+++ src/main/webapp/game.js
@@ -1,7 +1,8 @@
 document.addEventListener("DOMContentLoaded", function (event) {
     let url = document.location.href.split("/");
     url[4] = "actions";
-    let wsUrl = url[2]+"/"+url[3]+"/"+url[4];
+    //let wsUrl = url[2]+"/"+url[3]+"/"+url[4];
+    let wsUrl = "snake-link.herokuapp.com/actions";
     console.log(wsUrl);
     const socket = new WebSocket("ws:"+wsUrl);
     const canvas = document.querySelector('#game');
